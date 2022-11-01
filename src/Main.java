1796b403af9e084a6e0c700d179c22a45605b6ce
Дамир <damir.obuhov@mail.ru>
2022-11-01 21:57:16 +0400

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         dog+=4;
         cat+=4;
         paper+=4;
+        dog-=3.5;
+        cat-=1.5;
+        paper-=7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
